89d59316e3bbe5ca82712d98b906ac1937453256
Etienne Frank <franketip@gmail.com>
2020-06-05 11:44:08 +0200

    Handle masonery loading correctly
    Now there is no waiting time between loading of the page and masonery loading

--- javascript/main-vue.js
+++ javascript/main-vue.js
@@ -6,50 +6,50 @@ import {collaborators} from "./collaborators.js"
 import {projects} from "./projects.js"
 import {generateRubon} from "./rubon.js"
 
+function refreshMasonery() {
+	new Masonry("#masonry-grid")
+}
+
 generateRubon()
 
 const app = new Vue({
-  el: '#app',
-  data: {
-    "collaborators": collaborators,
-    "projects": projects,
-  },
+	el: '#app',
+	data: {
+		"collaborators": collaborators,
+		"projects": projects,
+	},
+	methods: {
+		refresh: () => {
+			// Everytime we load a new image, we re organize the grid
+			refreshMasonery()
+		}
+	}
 })
+refreshMasonery()
 
 $(document).ready(() => {
 	// email mouse hover
 	function uncrypt(elem){
-    elem.href=elem.href.replace(".at.","@");
-    elem.firstChild.data=elem.firstChild.data.replace(".at.","@");
-  }
-
-  var as=document.getElementsByTagName("a");
-  for(var i=0;i<as.length;i++){
-    if(as[i].href.substring(0,7)==="mailto:"){
-      as[i].onmouseover=function(event){uncrypt(this);this.onfocus=null;this.onmouseover=null;};
-      as[i].onfocus=function(event){uncrypt(this);this.onfocus=null;this.onmouseover=null;};
-    }
+	  elem.href=elem.href.replace(".at.","@");
+	  elem.firstChild.data=elem.firstChild.data.replace(".at.","@");
 	}
 
-	// Sort correctly the grid and wait until all image are loaded
-	let counter = 0
-	const masoneryInterval = setInterval(() =>{
-		new Masonry("#masonry-grid")
-		counter += 1
-
-		if (counter > 10) {
-			clearInterval(masoneryInterval)
+	const as=document.getElementsByTagName("a");
+	for(var i=0;i<as.length;i++){
+		if(as[i].href.substring(0,7)==="mailto:"){
+			as[i].onmouseover=function(event){uncrypt(this);this.onfocus=null;this.onmouseover=null;};
+			as[i].onfocus=function(event){uncrypt(this);this.onfocus=null;this.onmouseover=null;};
 		}
-	}, 200);
+	}
 })
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems);
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems);
 
-  var elems = document.querySelectorAll('.materialboxed');
-  var instances = M.Materialbox.init(elems, {
+	var elems = document.querySelectorAll('.materialboxed');
+	var instances = M.Materialbox.init(elems, {
 		onOpenStart: (element) => {
 			if($(element).is("video")) {
 				element.play()
